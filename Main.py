4d5389e9145611b10996f5f2ba21b79494ae4d60
Bakedonion46 <bakedonion46@proton.me>
2025-03-13 15:44:01 +0000

    fixed the .randGIF command

--- Main.py
+++ Main.py
@@ -4,21 +4,22 @@ import os
 import requests
 import random   
 import time
+from dotenv import load_dotenv
 
+load_dotenv()
+Token = os.getenv("TOKEN")
+GKEY = os.getenv("GKEY")
 intents = discord.Intents.all()
 intents.message_content = True
-
-Token = os.getenv("TOKEN")
-Tenkey = os.getenv("TENKEY")
-searchterm = "Funny meme"
-
 bot = commands.Bot(command_prefix=".", intents=intents)
+
 
 @bot.event
 async def on_ready():
     print("Hey {bot.user} is online")
-    print(Tenkey)
+    print(GKEY)
     print(Token)
+
 
 @bot.command()
 async def hello(ctx):
@@ -26,42 +27,41 @@ async def hello(ctx):
     time.sleep(0.5)
     await ctx.send("use .how to see all of my available commands")
 
+
 @bot.command()
 async def how (ctx):
     await ctx.send("My Current commands are: ")
     await ctx.send(".hello - Greet me ")
     await ctx.send(".MemCount - See the current member count")
+    await ctx.send(".randGIF - recieve a random gif")
+
 
 @bot.command() 
 async def MemCount(ctx):
     guild = ctx.guild
     await ctx.send(f'{guild.name} has {guild.member_count} members.')
 
+
 @bot.command()
-async def randGIF(ctx, *, search_term):
-    url = f"https://tenor.googleapis.com/v2/search?q={search_term}&key={Tenkey}&limit=10"
-    response = None
-
+async def randGIF(ctx, *, query: str = "Funny"):
+    await ctx.send("Here you go")
     try:
-        response = response.get(url)
-        response.raise_for_status()
-        gifs = response.json().get("Results")
+        url = f'https://api.giphy.com/v1/gifs/search'
+        params = {
+            "api_key": GKEY,
+            "q": query,
+            "limit": 10
+        }
+        response = requests.get(url, params = params)
+        data = response.json()
 
-        if gifs:
-            random_gif = random.choice(gifs)["url"]
-            await ctx.send(random_gif)
+        if data["data"]:
+            gif_url= random.choice(data["data"])["images"]["original"]["url"]
+            await ctx.send(gif_url)
         else:
-            await ctx.send("No available gifs man :(")
-    
-    except requests.exceptions.RequestException as e:
-        await ctx.send("Couldn't get any gifs call xane")
-        print(f'request failed: {e}')
-
-
+            await ctx.send("No GIF's Found")
     except Exception as e:
-        await ctx.send("call xane im fucked up")
-        print(f'request failed: {e}')
-
+        await ctx.send(f"Call xane im fucking tweaking and {e}")
 
 
 
