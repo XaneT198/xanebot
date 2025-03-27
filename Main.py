04ff0b174f6b45b53282e1c5f3403cf951b20d18
Xane <bakedoninon46@proton.me>
2025-03-27 11:51:24 +0000

    Updated the gamble feature to the bot

--- Main.py
+++ Main.py
@@ -1,68 +1,40 @@
 import discord
+import asyncio
 from discord.ext import commands
 import os
 import requests
 import random   
 import time
 from dotenv import load_dotenv
+import datetime
+import re
 
 load_dotenv()
-Token = os.getenv("TOKEN")
+Token = os.getenv("Token")
 GKEY = os.getenv("GKEY")
+
+async def load_extensions():
+    await bot.load_extension("cogs.how")
+    await bot.load_extension("cogs.hello")
+    await bot.load_extension("cogs.memcount")
+    await bot.load_extension("cogs.gamble")
+    await bot.load_extension("cogs.randgif")
+    await bot.load_extension("cogs.bal")  
+
 intents = discord.Intents.all()
 intents.message_content = True
-bot = commands.Bot(command_prefix=".", intents=intents)
 
+bot = commands.Bot(command_prefix=".", intents=intents)
 
 @bot.event
 async def on_ready():
     print("Hey {bot.user} is online")
     print(GKEY)
     print(Token)
+    await bot.tree.sync()
 
+async def main():
+    await load_extensions()
+    await bot.start(Token)
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Hey i'm xanebot programmed by Xane Thompson ")
-    time.sleep(0.5)
-    await ctx.send("use .how to see all of my available commands")
-
-
-@bot.command()
-async def how (ctx):
-    await ctx.send("My Current commands are: ")
-    await ctx.send(".hello - Greet me ")
-    await ctx.send(".MemCount - See the current member count")
-    await ctx.send(".randGIF - recieve a random gif")
-
-
-@bot.command() 
-async def MemCount(ctx):
-    guild = ctx.guild
-    await ctx.send(f'{guild.name} has {guild.member_count} members.')
-
-
-@bot.command()
-async def randGIF(ctx, *, query: str = "Funny"):
-    await ctx.send("Here you go")
-    try:
-        url = f'https://api.giphy.com/v1/gifs/search'
-        params = {
-            "api_key": GKEY,
-            "q": query,
-            "limit": 10
-        }
-        response = requests.get(url, params = params)
-        data = response.json()
-
-        if data["data"]:
-            gif_url= random.choice(data["data"])["images"]["original"]["url"]
-            await ctx.send(gif_url)
-        else:
-            await ctx.send("No GIF's Found")
-    except Exception as e:
-        await ctx.send(f"Call xane im fucking tweaking and {e}")
-
-
-
-bot.run(Token)
+asyncio.run(main())
